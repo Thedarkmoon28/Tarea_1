8139ecea13844efd71bd0c19491bdd87d3d90d8b
Thedarkmoon28 <mario.02.mav@gmail.com>
2026-02-23 21:24:41 -0600

    Ejercicio3

--- Tarea/src/main/java/com/example/tarea/Ejercicio03_When.kt
+++ Tarea/src/main/java/com/example/tarea/Ejercicio03_When.kt
@@ -21,8 +21,12 @@ fun main() {
             1 -> "Lunes"
             2 -> "Martes"
             3 -> "Miercoles"
+            4 -> "Jueves"
+            5 -> "Viernes"
+            6 -> "Sabado"
+            7 -> "Domingo"
             // TODO: Completa los dias restantes (4-7) y el else
-            else -> ""
+            else -> "Dia no valido"
         }
 
         println("$d -> $nombre")
